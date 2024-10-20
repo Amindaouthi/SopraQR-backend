b02fa84a1ab66c169182b7576aac5807b079480e
Amindaouthi <amin.daouthi@esprit.tn>
2024-10-20 09:37:36 -0400

    last update

--- src/main/java/com/bezkoder/spring/security/postgresql/CorsFilter.java
+++ src/main/java/com/bezkoder/spring/security/postgresql/CorsFilter.java
@@ -8,18 +8,31 @@ import javax.servlet.*;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
 
 @Component
 @Order(Ordered.HIGHEST_PRECEDENCE)
 public class CorsFilter implements Filter {
+
+    // List of allowed origins
+    private static final List<String> allowedOrigins = Arrays.asList(
+            "http://localhost:3000",
+            "http://localhost:3030"
+    );
 
     @Override
     public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
         HttpServletResponse response = (HttpServletResponse) res;
         HttpServletRequest request = (HttpServletRequest) req;
 
+        String origin = request.getHeader("Origin");
 
-        response.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+        // Check if the origin is in the list of allowed origins
+        if (allowedOrigins.contains(origin)) {
+            response.setHeader("Access-Control-Allow-Origin", origin);
+        }
+
         response.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
         response.setHeader("Access-Control-Allow-Headers", "*");
         response.setHeader("Access-Control-Allow-Credentials", "true");
